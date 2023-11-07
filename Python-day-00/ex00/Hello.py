3ca6311ee2b9500501317f508425113ea77bbf3f
Zineb Iloughmane <ziloughm@c3r6p9.1337.ma>
2023-11-07 23:35:49 +0100

    ex00 of day00

---
+++ Python-day-00/ex00/Hello.py
@@ -0,0 +1,11 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Morocco!")
+ft_set = {"Hello", "Benguerir!"}
+ft_dict = {"Hello" : "1337 Benguerir!"}
+
+ft_set = set(ft_set)
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
