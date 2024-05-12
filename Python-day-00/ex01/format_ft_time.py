804141f12e0d210483a3d3be17dd20335ff897ac
Zineb Iloughmane <ziloughm@c3r8p1.1337.ma>
2024-05-12 20:47:06 +0100

    rev

--- Python-day-00/ex01/format_ft_time.py
+++ Python-day-00/ex01/format_ft_time.py
@@ -1,6 +1,14 @@
+# Using datetime
 from datetime import datetime
 
 d = datetime.now()
 print(f"Seconds since January 1, 1970: {d.timestamp():,.4f} or \
 {d.timestamp():.2e} in scientific notation")
 print(d.strftime("%b %d %Y"))
+
+# Using time
+# import time 
+
+# s = time.time()
+# print(f"Seconds since January 1, 1970: {s:,.4f} or \
+# {s:.2e}  in scientific notation \n {time.strftime('%b %d %Y')}")
