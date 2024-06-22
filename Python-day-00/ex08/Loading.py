757e24d6d7a06518d9337662f36498f46b1704ee
zinb-il <zinb9419@gmail.com>
2024-06-22 17:03:51 +0100

    tqdm

--- Python-day-00/ex08/Loading.py
+++ Python-day-00/ex08/Loading.py
@@ -2,38 +2,47 @@ import os
 import time
 import sys
 
+def ft_hide_cursor():
+    '''Disable blinking cursor in control window'''
+    sys.stdout.write("\033[?25l")
+    sys.stdout.flush()
+
+def ft_show_cursor():
+    '''Disable blinking cursor in control window'''
+    sys.stdout.write("\033[?25h")
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
 
 def ft_tqdm(lst: range) -> None:
     '''Decorate an iterable object, returning an iterator which acts \
 exactly \nlike the original iterable, but prints a dynamically updating
 progressbar every time a value is requested.'''
-
+    # Le symbole à afficher dans la barre de recherche
     c = '\u2588'
     mx = len(lst)
-    lrg = os.get_terminal_size().columns - 25 - len(str(mx)) * 2
-    spt, tt, it, step = "00:00", '?', '?it/s', 0
-    # Turn off blinking cursor in command window
-    sys.stdout.write("\033[?25l")
-    sys.stdout.flush()
+    spt, tt, it, rmnt, cr = "00:00", '?', '?it/s', 0, ''
+    # Désactiver le curseur clignotant dans la fenêtre de commande
+    ft_hide_cursor()
     stl = time.time()
     for i in range(mx + 1):
+        lrg = os.get_terminal_size().columns - 25 - len(str(mx))
         p = str(i * 100 // mx).rjust(3)
-        ind = str(i).rjust(len(str(mx)))
+        ind = str(i)
         stop = time.time()
         st = int(stop - stl)
         spt = str(st // 60).zfill(2) + ':' + str(st % 60).zfill(2)
-        if i == 1:
-            tp = int(stop - stl) * mx
-            tt = str(tp // 60).zfill(2) + ':' + str(tp % 60).zfill(2)
-            step = round(float(stop - stl), 2)
-            lrg -= 9
         if i >= 1:
-            tp -= int(step)
-            tt = str(tp // 60).zfill(2) + ':' + str(tp % 60).zfill(2)
-            it = f'{ (i + 1) / (stop - stl):.2f}s/it'.rjust(9)
+            rmnt = i / st if st else 0
+            cr = 'it/s' if rmnt > 1 else 's/it'
+            it = f'{rmnt:.2f}{cr}'.rjust(9)
+            rmnt = int((mx - i) / rmnt) if rmnt else 0
+            tt = str(rmnt // 60).zfill(2) + ':' + str(rmnt % 60).zfill(2)
+            lrg -= len(it) - 6 + len(tt)
+        lrg -= len(ind)
         lp = (c * (i * lrg // mx)).ljust(lrg)
         print(f'{p}%|{lp}| {ind}/{mx} [{spt}<{tt}, {it}]', end='\r')
         if i == mx:
-            sys.stdout.write("\033[?25h")
-            sys.stdout.flush()
+            # Activer le curseur  dans la fenêtre de commande
+            ft_show_cursor()
         yield i
--- Python-day-00/ex08/test.py
+++ Python-day-00/ex08/test.py
@@ -6,15 +6,30 @@ from Loading import ft_tqdm
 
 
 # print(tqdm.__doc__)
+
+# # Test1
+for elem in ft_tqdm(range(10)):
+    sleep(0.5)
+print()
+for elem in tqdm(range(10)):
+    sleep(0.5)
+print()
+
+
+# # Test2
 for elem in ft_tqdm(range(333)):
-    sleep(0.005)
+    sleep(0.05)
 print()
 for elem in tqdm(range(333)):
-    sleep(0.005)
+    sleep(0.05)
 print()
 
 
-# colors = ["Blue","Green","Yellow","White","Gray","Black"]
-# for x in tqdm(colors):
-#     sleep(1)
-#     print(x)
+# # Test3
+colors = ["Blue","Green","Yellow","White","Gray","Black"]
+
+for x in tqdm(colors):
+    sleep(1)
+
+for x in ft_tqdm(colors):
+    sleep(1)
