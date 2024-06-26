05c620a5cac85ed9fb99365ec9960b04aec3362f
zinb-il <zinb9419@gmail.com>
2024-06-26 18:11:42 +0100

    rev

--- Python-day-00/ex01/format_ft_time.py
+++ Python-day-00/ex01/format_ft_time.py
@@ -10,5 +10,6 @@
 import time
 
 s = time.time()
-print(f"Seconds since January 1, 1970: {s:,.4f} or \
-{s:.2e}  in scientific notation \n {time.strftime('%b %d %Y')}")
+print(f"Seconds since January 1, 1970: {s:,.4f} or {s:.2e} \
+in scientific notation")
+print(f"{time.strftime('%b %d %Y')}")
--- Python-day-00/ex09/build/lib/ft_package/__init__.py
+++
@@ -1,1 +0,0 @@
-from .count_in_list import count_in_list
